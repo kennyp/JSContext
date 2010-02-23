1f6511db401fb4fb4b7d41171b29a260f1c22fd2
Kenny Parnell <k.parnell@gmail.com>
2010-02-23 14:15:00 -0500

    added license and copyright notice

---
+++ README.markdown
@@ -0,0 +1,7 @@
+JSContext
+=========
+
+JSContext is a library that allows people to create a context/sandbox
+for loading different library versions into the same page.
+
+See demo.html for an example.
--- context.js
+++ context.js
@@ -1,3 +1,20 @@
+/**
+ * JSContext
+ * Copyright (C) 2010 Kenny Parnell <kparnell@redventures.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 if (typeof Array.prototype.each === 'undefined') {
     Array.prototype.each = function (func) {
         for (var i = 0; i < this.length; i += 1) {
