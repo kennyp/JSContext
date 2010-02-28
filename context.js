46c0921e83c25683c3b5fb9834db83e99f4e8c31
Kenny Parnell <k.parnell@gmail.com>
2010-02-28 13:01:07 -0500

    Corrected email address.

--- context.js
+++ context.js
@@ -1,6 +1,6 @@
 /**
  * JSContext
- * Copyright (C) 2010 Kenny Parnell <kparnell@redventures.com>
+ * Copyright (C) 2010 Kenny Parnell <k.parnell@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published 
